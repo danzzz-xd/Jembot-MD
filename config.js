f54b8566acabdf90ce4d21905daf2e3b5bc081ae
Kkkakshsj <99302496+Kkkakshsj@users.noreply.github.com>
2022-02-27 01:55:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6289512545999'] // Put your number here
+global.owner = ['6282325673016'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -30,9 +30,9 @@ global.APIKeys = { // APIKey Here
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'punya... '
 global.author = ' antum'
-global.email = 'Ramdani Official@hotmail'
+global.email = 'Delta@hotmail'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© Jembot-MD'
+global.wm = '© Delta'
 global.media = 'https://telegra.ph/file/d57aa7b4fbecbcbd303df.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
